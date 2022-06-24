e9e851a3b25fc635b216a3330fc812a6d1669621
Yu-Huan Wu <wuyuhuan@mail.nankai.edu.cn>
2022-06-24 15:47:59 +0800

    Update train.sh
    Add some training details and advice

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+#### Default training script is for ResNet-50
 PYTHONPATH=$(pwd):$PYTHONPATH python3 scripts/train.py --max_epochs 30 \
                                                     --num_workers 2 \
-                                                    --batch_size 24 \
-                                                    --savedir ./snaps/edn-lite \
+                                                    --batch_size 12 \
+                                                    --savedir ./snaps/edn-resnet50 \
                                                     --lr_mode poly \
                                                     --lr 5e-5 \
                                                     --width 384 \
                                                     --height 384 \
-                                                    --iter_size 1 \
-                                                    --arch mobilenetv2 \
+                                                    --iter_size 2 \
+                                                    --arch resnet50 \
                                                     --ms 1 \
                                                     --ms1 0 \
                                                     --bcedice 1 \
@@ -17,11 +18,20 @@ PYTHONPATH=$(pwd):$PYTHONPATH python3 scripts/train.py --max_epochs 30 \
                                                     --group_lr 0 \
                                                     --freeze_s1 0
 
-##### The true batchsize is batch_size * iter_size, which is (24 * 1) here.
+##### The true batchsize is batch_size * iter_size, which is (12 * 2) here.
 ##### GPU with 12G memory:
 ##### For edn-vgg16,  you can use "--batch_size 6 --iter_size 4 --arch vgg16"
 ##### For edn-resnet, you can use "--batch_size 12 --iter_size 2 --arch resnet50"
-##### For edn-lite, you can use "--batch_size 24 --iter_size 1 --arch mobilenetv2"
+##### For edn-lite, you can use "--lr 1.7e-4 --batch_size 24 --iter_size 1 --arch mobilenetv2"
+##### Please ensure batchsize >= 6, since a very small batchsize may significantly decrease the performance.
 
 ##### GPU <8G memory
 ##### If your gpu memory is not enough, you can reduce --batch_size and increase --iter_size, keeping batch_size*iter_size =24 or near 24.
+
+##### Multi-scale training strategy
+##### We support two strategies: ms and ms1
+##### --ms: train with one scale for a long time, and then train with next scale.
+##### --ms1: train with random scale for each iteration
+##### I think --ms1 should work better than than --ms, but experiments show that they can achieve comparable performance. 
+##### If you wanna use ms1 strategy, please set `--ms 0 --ms1 1 --max_epochs to 90`.
+
